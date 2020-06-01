d7f0b6b15c3cb5253d14fe7316b5796fc379fd08
timlee0119 <b05705006@ntu.edu.tw>
2020-06-01 18:35:39 +0800

    inmission popup styling, except trend

--- public/chrome/popup/js/inMission.js
+++ public/chrome/popup/js/inMission.js
@@ -1,5 +1,12 @@
 var initRender = true;
 
+function onCollapseClick(more, collapsible, grow = true) {
+  return function () {
+    more.style.display = grow ? 'none' : 'flex';
+    collapsible.style.height = grow ? `${collapsible.scrollHeight}px` : '0';
+  };
+}
+
 function setContent(node, content) {
   document.querySelector(node).innerHTML = content;
 }
@@ -13,7 +20,7 @@ function getWebsiteIcons(websites) {
   var ret = '';
   websites.forEach(w => {
     // dangerous!! Could be XSS
-    ret += `<span><img height="16" width="16" src='http://www.google.com/s2/favicons?domain=${w}'/></span>`;
+    ret += `<span title=${w}><img height="16" width="16" src='http://www.google.com/s2/favicons?domain=${w}'/></span>`;
   });
   return ret;
 }
@@ -31,7 +38,7 @@ function renderMission(data) {
     var par = participants[w];
     var his = par.usageHistory;
     if (initRender) {
-      setContent('#mission_name', name);
+      setContent('#mission_name', `進行中任務：${name}`);
       setContent(`#name_${w}`, par.name);
       setContent(`#limitTime_${w}`, getTime(par.limitTime));
       setContent(`#limitedWebsites_${w}`, getWebsiteIcons(par.limitedWebsites));
@@ -39,9 +46,14 @@ function renderMission(data) {
       document.querySelector('#content').style.display = 'block';
       document.querySelector('#loading').style.display = 'none';
     }
-    setContent(`#todayUsage_${w}`, getTime(his[his.length - 1]));
+    var limitTime = getTime(par.limitTime);
+    var todayTime = getTime(his[his.length - 1]);
+    setContent(`#todayUsage_${w}`, todayTime);
+    if (todayTime >= limitTime) {
+      document.querySelector(`#todayUsage_${w}`).classList.add('text-error');
+    }
     setContent(`#successDay_${w}`, par.successDay);
-    setContent(`#bonus_${w}`, par.bonus);
+    setContent(`#bonus_${w}`, `+ ${par.bonus}`);
   });
 
   initRender = false;
@@ -50,4 +62,18 @@ function renderMission(data) {
 window.onload = function () {
   var port = chrome.runtime.connect({ name: 'getMissionStatus' });
   port.onMessage.addListener(renderMission);
+
+  var moreBtns = document.querySelectorAll('.btn-more_info');
+  var lessBtns = document.querySelectorAll('.btn-less_info');
+  var collapsibles = document.querySelectorAll('.collapsible');
+  for (var i = 0; i < moreBtns.length; ++i) {
+    moreBtns[i].addEventListener(
+      'click',
+      onCollapseClick(moreBtns[i], collapsibles[i])
+    );
+    lessBtns[i].addEventListener(
+      'click',
+      onCollapseClick(moreBtns[i], collapsibles[i], false)
+    );
+  }
 };
